278fe696f31c15d6ba3153ceb822a3139f8d270c
bryanphilips <bphilips.devops@gmail.com>
2024-06-29 22:56:35 +0300

    test branch working well

--- _config.js
+++ _config.js
@@ -5,6 +5,7 @@ config.mongoURI = {
     production: 'mongodb+srv://philips:root@moringa0.0to2jfw.mongodb.net/?retryWrites=true&w=majority&appName=Moringa0',
     development: 'mongodb+srv://philips:root@moringa0.0to2jfw.mongodb.net/?retryWrites=true&w=majority&appName=Moringa0',
     test: 'mongodb+srv://philips:root@moringa0.0to2jfw.mongodb.net/?retryWrites=true&w=majority&appName=Moringa0',
+    
    
 }
 module.exports = config;
